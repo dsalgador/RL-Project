af62e002b4293674a1bc177d567308c77fbe158a
dsalgador <daniel.salgado.rojo@gmail.com>
2018-03-26 12:22:03 +0200

    Add dload_level_percentages property to tanks

--- tank.py
+++ tank.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 class Tank():
-    def __init__(self, tank_id, current_load, max_load, consumption_rate, n_discrete_load_levels):
+    def __init__(self, tank_id, current_load, max_load, consumption_rate, n_discrete_load_levels,
+                load_level_percentages):
         self.id = tank_id
         self.load = current_load
         self.max_load = max_load
         self.rate = consumption_rate
         self.levels = np.linspace(0,self.max_load, n_discrete_load_levels+1)[1:]
+        self.level_percentages = load_level_percentages
    
     def fill(self):
         self.load = self.max_load    
